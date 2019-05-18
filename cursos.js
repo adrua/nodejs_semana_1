1697e68834ace71f8473640956cf21b57948e997
adrua <adrua@hotmail.com>
2019-05-18 18:37:00 -0500

    HU: Preinscripciones

--- cursos.js
+++ cursos.js
@@ -3,19 +3,29 @@ const cursos = [
         id: 101,
         nombre: 'Introduccion a la Mecanica Quantica',
         duracion: '10 semanas',
-        valor: 25000
+        valor: 25000,
+        preinscritos: []
     },
     {
         id: 203,
         nombre: 'Teoria de Cuerdas                  ',
         duracion: '20 semanas',
-        valor: 33000
+        valor: 33000,
+        preinscritos: []
     },
     {
         id: 401,
         nombre: 'Computacion Quantica               ',
         duracion: ' 8 semanas',
-        valor: 15000
+        valor: 15000,
+        preinscritos: []
+    },
+    {
+        id: 402,
+        nombre: 'operadores Quanticas              ',
+        duracion: ' 6 semanas',
+        valor: 38000,
+        preinscritos: []
     }
 ]
 
@@ -35,6 +45,23 @@ function mostrarCursos() {
     }, 2000)
 }
 
+function mostrarPreinscritos() {
+    console.log('------------------------------------------');
+    console.log(`Cursos Disponible ${new Date()}`);
+    console.log('------------------------------------------');
+    cursos.forEach((curso) => console.log(`${curso.id}\t${curso.nombre}\t${curso.preinscritos.length}`));
+}
+
+function preinscripcion(codigoCurso, estudiante) {
+    const curso = cursos.find((x) => x.id === codigoCurso);
+    if(curso) {
+        curso.preinscritos.push(estudiante);
+    } 
+    return curso;
+}
+
 module.exports = {
-    mostrarCursos    
+    mostrarCursos,
+    mostrarPreinscritos,
+    preinscripcion
 };
--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-const {mostrarCursos} = require('./cursos');
+const opcionesMostrarCursos = {
+};
 
+const opcionesPreinscripcion = {
+    estudiante: {
+        demand: true,
+        alias: 'e'
+    },
+    curso: {
+        demand: true,
+        alias: 'c'
+    }
+};
 
-mostrarCursos();
+const argv = require('yargs')
+        .command('mostrar-cursos', 'Lista de cursos disponible', opcionesMostrarCursos)
+        .command('preinscripcion', 'preincripcion curso', opcionesPreinscripcion)
+        .argv;
+
+const misCursos = require('./cursos');
+
+console.log(argv);
+switch(argv._[0]) {
+    case 'mostrar-cursos':
+        misCursos.mostrarCursos();
+        break;
+    case 'preinscripcion':
+        if(misCursos.preinscripcion(argv.curso, argv.estudiante)) {
+            misCursos.mostrarPreinscritos();                        
+        } else {
+            console.log(`Curso: ${argv.curso} no existe`);
+        };
+        break;    
+}
